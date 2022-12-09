4a10b687b1338fceeef4e8b54ed106949114a1fa
Sydney Shillinglaw <76266186+sshilli@users.noreply.github.com>
2022-12-09 18:18:21 -0500

    Update twitterAPI2.py

--- twitterAPI2.py
+++ twitterAPI2.py
@@ -1,13 +1,16 @@
 import tweepy
 
-consumer_key = "o99Nn7p75iCgu2wR6M9b4Slvf"
-consumer_secret = "GkRkrZlknxq2PMXcc4Sp5x0tPHFHFP5JD9zV3oWYnmQ5oZcyVt"
-access_token = "RC1lSEhIbk54V0lBbUllQkhBUkE6MTpjaQ"
-access_token_secret = "XqAxFtFd7dt_QCUyY-WfUoJw-0aCN8a03owGw7_2unKW40eQGY"
+CONSUMER_KEY = "249E5GLxUhPCo8tKmi27R2Hdh"
+CONSUMER_SECRET = "qWWBMwsHTvQDHF8GiA2H4vaszWilVpLH9LwtEkjEjuviqdI2R1"
+ACCESS_TOKEN = "1178395130467733504-Yqu6RKsAO8flAwedtHFQwU2TR0mCCS"
+ACCESS_TOKEN_SECRET = "bivNLppWzRFfn8tZMXPmgUI9gehVxGm3wtMsyzsLZep3P"
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAI5ekAEAAAAATsu9FIydLn%2Fs0omflv0LzVu9pE4%3DnhOLu8xSz9TBJAfzlNK3HPY8cLvjFFbvzo3eIIoOc9tUtO1RKA"
 
 def tweet(images, weekday):
-    client = tweepy.Client(access_token["access_token"])
+    api = tweepy.Client(bearer_token=BEARER_TOKEN, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET, 
+    consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
 
-    response = client.create_tweet( text= weekday + "'s Photos!",
+    api.create_tweet(text= weekday + "'s Photos!",
         media_ids=images)
-    print(f"https://twitter.com/user/status/{response.data['id']}")
+
+
